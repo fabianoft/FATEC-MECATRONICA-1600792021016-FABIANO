089beb084f9c70898a0c04d761352568ae898c82
fabianoft <70581703+fabianoft@users.noreply.github.com>
2020-09-22 08:21:52 -0300

    Create programa01.py

---
+++ LTP1-2020-2/pratica05/programa01.py
@@ -0,0 +1,14 @@
+#Exercício 1 - Aula 05
+#Le 3 valores e encontra o maior, menor e a média deles.
+
+valor1 = int(input('Informe um valor:'))
+valor2 = int(input('Informe outro valor:'))
+valor3 = int(input('Informe um 3 valor:'))
+
+#Encontrar o maior valor
+
+#Encontrar o menor valor
+
+#Encontrar a média
+media = (valor1 + valor2 + valor3) / 3
+print('Valor médio:', media)
